f400c38766144ab4c228f99eca1066a9ea6a6067
vagrant <vagrant@localhost.localdomain>
2018-09-25 01:11:52 +0900

    bootstrap

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,10 +4,12 @@
 	<meta charset="utf-8">
 	<title>@yield('title')</title>
 	<link rel="stylesheet" href="/css/styles.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 <body>
   <div class="container">
 	@yield('content')
   </div>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
 </html>
