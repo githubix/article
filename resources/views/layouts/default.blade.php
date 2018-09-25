01e5a7c7203cef72365deb6d8bde4f6cc8a40aad
vagrant <vagrant@localhost.localdomain>
2018-09-25 09:00:22 +0900

    bootstrap

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>@yield('title')</title>
-	<link rel="stylesheet" href="/css/styles.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
   <div class="container">
